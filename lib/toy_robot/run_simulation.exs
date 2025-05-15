f3ec1d6d2ff0452473b0e619a820b0a8737ab9fb
Felipe Moura <felipemoura1103@gmail.com>
2025-05-15 17:15:48 -0300

    Update run_simulation.exs

--- lib/toy_robot/run_simulation.exs
+++ lib/toy_robot/run_simulation.exs
@@ -1,11 +1,1 @@
-alias ToyRobot.CommandInterpreter
-alias ToyRobot.CommandRunner
 
-commands = (
-  "commands.txt"
-  |> File.read!()
-  |> String.split("\n", trim: true)
-  |> CommandInterpreter.interpret()
-)
-
-ToyRobot.CommandRunner.run(commands)
